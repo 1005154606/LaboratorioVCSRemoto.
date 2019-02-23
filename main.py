7600f58b46bbf104f780f993cc0d1b7ef5c2a6ab
unknown <wilson.escobar.2019@upb.edu.co>
2019-02-22 21:27:16 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print("Primer ejercicio")
-a=int(input("Introduce el primer número: "))
-c=int(input("Introduce el segundo número: "))
-producto=a*c
-print("el producto de", a ,"y", c ,"es", producto)
-doble_a=2*a
-print("el doble de", a , "es", doble_a)
+print("segundo ejercicio")
+b=int(input("Introduce el primer número: "))
+cuadrado_b=b**2
+print("El cuadrado de", b , "es" ,cuadrado_b)
+import math
+d=int(input("Ingrese el segundo número: "))
+raiz_d=math.sqrt(d)
+print("la raiz cuadrada de", d ,"es", raiz_d)

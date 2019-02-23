413733be6bcc653fb9919e0dfc9d3113035acff5
unknown <wilson.escobar.2019@upb.edu.co>
2019-02-22 21:13:34 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+print("Primer ejercicio")
+a=int(input("Introduce el primer número: "))
+c=int(input("Introduce el segundo número: "))
+producto=a*c
+print("el producto de", a ,"y", c ,"es", producto)
+doble_a=2*a
+print("el doble de", a , "es", doble_a)
